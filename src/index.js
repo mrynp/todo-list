9c94589cae77036a771cb6d579a62f2bfa7a01f5
mrynp <110476086+mrynp@users.noreply.github.com>
2024-10-29 12:49:00 +0800

    Initialize todo list app structure with basic functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,68 @@
 import "./styles.css";
 
-const header = document.createElement("h1");
-header.textContent = "TODO LIST";
-document.body.appendChild(header);
+const todoList = [];
+
+function Todo(title, status) {
+  this.title = title;
+  this.status = status;
+}
+
+function addToTodoList(title, status) {
+  todoList.push(new Todo(title, status));
+  displayTodo;
+}
+
+function displayNav() {
+  const nav = document.createElement("nav");
+
+  const todoAll = document.createElement("button");
+  todoAll.textContent = "All tasks";
+
+  const todoToday = document.createElement("button");
+  todoToday.textContent = "Today's tasks";
+
+  nav.appendChild(todoAll);
+  nav.appendChild(todoToday);
+
+  return nav;
+}
+
+function displayTodo() {
+  const todoContainer = document.createElement("div");
+  todoContainer.classList.add("todo-container");
+
+  const todoTitle = document.createElement("h1");
+  todoTitle.textContent = "Todo Today";
+
+  const todoListContainer = document.createElement("div");
+  todoListContainer.classList.add("todo-list-container");
+  todoListContainer.innerHTML = "";
+
+  todoList.forEach((todo, index) => {
+    const div = document.createElement("div");
+    div.className = "todo";
+    div.innerHTML = `<p class="todo-title">${todo.title}</p>`;
+
+    todoListContainer.appendChild(div);
+  });
+
+  todoContainer.appendChild(todoTitle);
+  todoContainer.appendChild(todoListContainer);
+
+  return todoContainer;
+}
+
+function displayTodoApp() {
+  const main = document.createElement("main");
+  const nav = displayNav();
+  const todoContainer = displayTodo();
+
+  main.appendChild(nav);
+  main.appendChild(todoContainer);
+
+  document.body.appendChild(main);
+}
+
+addToTodoList("read book", false);
+addToTodoList("walk the dog", false);
+displayTodoApp();
